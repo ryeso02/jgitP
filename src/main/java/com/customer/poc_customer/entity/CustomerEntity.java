10065174f4fc745869b91ef56928976ce03c7ed4
RYeso <RYeso@OPT-LAP-0617.Opteamix.COM>
2023-09-08 11:53:37 +0530

    changes in CustomerEntity added

--- src/main/java/com/customer/poc_customer/entity/CustomerEntity.java
+++ src/main/java/com/customer/poc_customer/entity/CustomerEntity.java
@@ -9,38 +9,46 @@ import jakarta.persistence.Table;
 
 @Entity
 public class CustomerEntity {
-	
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;   
-	    
-	    private String name;
-	    private String address;
-	    private int age;
-		public Long getId() {
-			return id;
-		}
-		public void setId(Long id) {
-			this.id = id;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getAddress() {
-			return address;
-		}
-		public void setAddress(String address) {
-			this.address = address;
-		}
-		public int getAge() {
-			return age;
-		}
-		public void setAge(int age) {
-			this.age = age;
-		}
-	 
-			
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String address;
+	private int age;
+	private String firstName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
