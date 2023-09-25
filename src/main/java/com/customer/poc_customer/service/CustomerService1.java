eecfbc1c78d15ba2e7beb6839a6f12fbabff5620
Rahul Yeso <RYeso@Opteamix.com>
2023-09-25 14:42:05 +0530

    add CustomerService1.java

--- src/main/java/com/customer/poc_customer/service/CustomerService1.java
+++ src/main/java/com/customer/poc_customer/service/CustomerService1.java
@@ -1,43 +1,44 @@
-package com.customer.poc_customer.service;
+// package com.customer.poc_customer.service;
 
-public
-package com.customer.poc_customer.service;
+// public
+// package com.customer.poc_customer.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.customer.poc_customer.entity.CustomerEntity;
-import com.customer.poc_customer.repository.CustomerRepository;
+// import com.customer.poc_customer.entity.CustomerEntity;
+// import com.customer.poc_customer.repository.CustomerRepository;
 
-@Service
-public class CustomerService {
+// @Service
+// public class CustomerService {
 
-   private final CustomerRepository customerRepo;
+// private final CustomerRepository customerRepo;
 
-   @Autowired
-   public CustomerService(CustomerRepository customerRepo) {
-       this.customerRepo = customerRepo;
-   }
+// @Autowired
+// public CustomerService(CustomerRepository customerRepo) {
+// this.customerRepo = customerRepo;
+// }
 
-   public CustomerEntity createCustomer1(CustomerEntity customer) {
-       return customerRepo.save(customer);
-   }
-   
-   public CustomerResponseDTO createCustomer(CustomerEntity customer) {
-       CustomerEntity savedCustomer = customerRepository.save(customer);
-       String message = "Customer created successfully";
-       
-       // Create and return the DTO
-       CustomerResponseDTO responseDTO = new CustomerResponseDTO();
-       responseDTO.setMessage(message);
-       responseDTO.setCustomer(savedCustomer);
-       
-       return responseDTO;
-   }
+// public CustomerEntity createCustomer1(CustomerEntity customer) {
+// return customerRepo.save(customer);
+// }
 
-   // You can add more methods for fetching, updating, or deleting customers if needed
-}
+// public CustomerResponseDTO createCustomer(CustomerEntity customer) {
+// CustomerEntity savedCustomer = customerRepository.save(customer);
+// String message = "Customer created successfully";
 
-{
+// // Create and return the DTO
+// CustomerResponseDTO responseDTO = new CustomerResponseDTO();
+// responseDTO.setMessage(message);
+// responseDTO.setCustomer(savedCustomer);
 
-}
+// return responseDTO;
+// }
+
+// // You can add more methods for fetching, updating, or deleting customers if
+// needed
+// }
+
+// {
+
+// }
